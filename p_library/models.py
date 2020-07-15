59befb4faeae058e43a83edf24561999bcc5c69b
Semergal <37205006+Semergal@users.noreply.github.com>
2020-07-15 14:48:28 +1000

    New

---
+++ p_library/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class Author(models.Model):
+    full_name = models.TextField()
+    birth_year = models.SmallIntegerField()
+    country = models.CharField(max_length=2)
+    def __str__(self):
+        return self.full_name
+
+class Publisher(models.Model):
+    name = models.TextField()
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    ISBN = models.CharField(max_length=13)
+    title = models.TextField()
+    description = models.TextField()
+    year_release = models.SmallIntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, null=True, on_delete=models.CASCADE, blank=True, related_name='books')
+    copy_count = models.SmallIntegerField(default=1)
+    price = models.DecimalField(default = None, max_digits=6, decimal_places=2)
+    def __str__(self):
+        return self.title
+
+
+
+
